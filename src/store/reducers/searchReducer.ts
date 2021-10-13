bf317173986006dc78a2c55c6f16062c28daeb71
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2021-10-13 19:55:31 +0530

    redux implementation

---
+++ src/store/reducers/searchReducer.ts
@@ -0,0 +1,11 @@
+const initialState = {};
+
+export const searchReducer = (
+  state = initialState,
+  action: { type: string; payload: any }
+) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
